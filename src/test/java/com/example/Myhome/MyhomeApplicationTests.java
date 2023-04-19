804861bbff89c5b2508b1c4ac6fe77cea3aac0a3
ndil <nohdil88@ulim.co>
2023-04-19 10:24:04 +0900

    init

---
+++ src/test/java/com/example/Myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
